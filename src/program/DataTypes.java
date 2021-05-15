f98206c62456976e4e1369521eff093cc9c6ec43
mandu <mandushegunshi@gmail.com>
2021-05-15 17:29:12 +0000

    Implicit and Explicit Datatypes

---
+++ src/program/DataTypes.java
@@ -0,0 +1,24 @@
+package program;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//--implicit type---
+		char a='a';
+		System.out.println("Value: "+a);
+		
+		int b='b';
+		
+		System.out.println("Value: "+b);	
+
+		
+		//----explicit type---
+		double num=45.5;
+		
+	int num1=(int) num;
+	System.out.println("Value: "+num1);
+	}
+
+}
