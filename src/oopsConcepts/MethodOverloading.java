f87072fe09ec9ec06eee361d95b59e8bcbb6be01
mandu <mandushegunshi@gmail.com>
2021-05-16 17:42:36 +0000

    MethodOverloading

---
+++ src/oopsConcepts/MethodOverloading.java
@@ -0,0 +1,17 @@
+package oopsConcepts;
+
+public class MethodOverloading {
+	
+	public void add(int a,int b)
+	{
+		int sum=a+b;
+		System.out.println("Integer sum:" +sum);
+		
+	}
+	
+	public void add1(double a,double b)
+	{
+		double sum=a+b;
+		System.out.println("Integer sum:" +sum);
+	}
+}
